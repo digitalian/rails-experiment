5fe7affd052aed9027d77d7311c59374afa4162a
SHOICHI Shimizu <shoichi_shimizu@worksjapan.co.jp>
2014-08-27 17:26:28 +0900

    heroku update

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140812093211) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "addresses", force: true do |t|
     t.integer  "customer_id",                null: false
     t.string   "type",                       null: false
