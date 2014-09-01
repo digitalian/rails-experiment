ef846e1fe14506d102078643bb15f4df981bcf85
SHOICHI Shimizu <shoichi_shimizu@worksjapan.co.jp>
2014-09-01 16:41:07 +0900

    applicants administration added

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,5 +1,5 @@
 class Program < ActiveRecord::Base
-  has_many :entries, dependent: :destroy
+  has_many :entries, dependent: :restrict_with_exception
   has_many :applicants, through: :entries, source: :customer
   belongs_to :registrant, class_name: 'StaffMember'
 
@@ -26,6 +26,11 @@ class Program < ActiveRecord::Base
       less_than_or_equal_to: 1000, allow_blank: true
   }
 
+  validates :max_number_of_participants, numericality: {
+      only_integer: true, greater_than_or_equal_to: 1,
+      less_than_or_equal_to: 1000, allow_blank: true
+  }
+
   validate do
     if min_number_of_participants && max_number_of_participants && min_number_of_participants > max_number_of_participants
      errors.add(:max_number_of_participants, :less_than_min_number)
@@ -38,6 +43,10 @@ class Program < ActiveRecord::Base
       .group('programs.id')
       .order(application_start_time: :desc).includes(:registrant)
   }
+
+  def deletable?
+    entries.empty?
+  end
   
   private
   def set_application_start_time
